4f62562919d0bf155ac776b5b78261450ce52a17
ryampeguero <ryampeguero@gmail.com>
2024-05-08 12:19:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/vue-fontawesome';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+  components: {
+    "font-awesome-icon": FontAwesomeIcon,
+  },
+  created(){
+    console.log(FontAwesomeIcon);
+  }
+}
+</script>
+
+<template>
+  <font-awesome-icon :icon="['fas', 'house']" />
+</template>
